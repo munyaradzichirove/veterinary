e3b0915f702010a8c3e664afd573cfb79ce38a35
munyaradzi <chirovemunyaradzi@gmail.com>
2025-12-12 16:53:07 +0200

    new docs

---
+++ veterinary/veterinary/doctype/patient_name/test_patient_name.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, chirovemunyaradzi@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPatientName(FrappeTestCase):
+	pass
---
+++ veterinary/veterinary/doctype/pet_breed/pet_breed.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, chirovemunyaradzi@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Pet Breed", {
+// 	refresh(frm) {
+
+// 	},
+// });
